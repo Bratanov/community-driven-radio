c25b65c34a4486a5b98439eb05d688f729ff1dab
Veselin Bratanov <veselin@devlabs.bg>
2016-02-08 11:01:22 +0200

    Changed: Code formatting for the client side script, was pretty out of control

--- public/badass-client.js
+++ public/badass-client.js
@@ -1,112 +1,111 @@
+var socket = io.connect('/');
 
-	var socket = io.connect('/');
+$('#message').on('keyup', function(e){
+	if(e.keyCode == 13){
+		var message = $('#my-name').val() + ' - ' + $(this).val();
 
-	$('#message').on('keyup', function(e){
-		if(e.keyCode == 13){
-			var message = $('#my-name').val() + ' - ' + $(this).val();
+		//Send msg
+		socket.emit('chat_msg', message);
+		
+		addMessage(message);
 
-			//Send msg
-			socket.emit('chat_msg', message);
-			
-			addMessage(message);
+		//Clear text in input
+		$(this).val('');
+	}
+});
 
-			//Clear text in input
-			$(this).val('');
-		}
-	});
+$('#newSong').on('keyup', function(e){
+	if(e.keyCode == 13){
+		//Send msg
+		socket.emit('new_song', $(this).val());
 
-	$('#newSong').on('keyup', function(e){
-		if(e.keyCode == 13){
-			//Send msg
-			socket.emit('new_song', $(this).val());
+		//Clear text in input
+		$(this).val('');
+	}
+});
 
-			//Clear text in input
-			$(this).val('');
-		}
-	});
+$('body').on('click', '.btn-vote', function() {
+	socket.emit('vote', $(this).data('song-id'));
+});
 
-	$('body').on('click', '.btn-vote', function() {
-		socket.emit('vote', $(this).data('song-id'));
-	});
+$('body').on('click', '.add-song', function() {
+	socket.emit('new_song', $(this).data('youtube-id'));
+});
 
-	$('body').on('click', '.add-song', function() {
-		socket.emit('new_song', $(this).data('youtube-id'));
-	});
-	
-	socket.on('chat_msg', function(data){
-		addMessage(data);
-	});
+socket.on('chat_msg', function(data){
+	addMessage(data);
+});
 
-	socket.on('new_song', function(song) {
-		$('#text').prepend('<p>Now plaing: ' + song + '</p>');
-		play(song);
-		console.log(song);
-	});
+socket.on('new_song', function(song) {
+	$('#text').prepend('<p>Now plaing: ' + song + '</p>');
+	play(song);
+	console.log(song);
+});
 
-	socket.on('usersCount', function(data){
-		$('#usersCount').text(data);
-	});
+socket.on('usersCount', function(data){
+	$('#usersCount').text(data);
+});
 
-	socket.on('getRefreshed', function(data){
-		window.location.reload();
-	});
+socket.on('getRefreshed', function(data){
+	window.location.reload();
+});
 
-	socket.on('song_info', function(data) {
-		$('#song-info').html(renderSong(data));
-	});
+socket.on('song_info', function(data) {
+	$('#song-info').html(renderSong(data));
+});
 
-	socket.on('queue_info', function(data) {
-		$queue_info = $('#queue-info');
-		$queue_info.html('<ol></ol>');
-		$queue_info = $queue_info.find('ol');
+socket.on('queue_info', function(data) {
+	$queue_info = $('#queue-info');
+	$queue_info.html('<ol></ol>');
+	$queue_info = $queue_info.find('ol');
 
-		for(var song in data) {
-			$queue_info.append('<li>' + renderSong(data[song]) + '</li>');
-		}
-	});
+	for(var song in data) {
+		$queue_info.append('<li>' + renderSong(data[song]) + '</li>');
+	}
+});
 
-	socket.on('be_alerted', function(message) {
-		// alert lolzorz
-		alert(message);
-	});
+socket.on('be_alerted', function(message) {
+	// alert lolzorz
+	alert(message);
+});
 
-	socket.on('related_info', function(data) {
-		var relatedView = renderRelatedSongs(data);
+socket.on('related_info', function(data) {
+	var relatedView = renderRelatedSongs(data);
 
-		$('#related-songs').html(relatedView);
-	});
+	$('#related-songs').html(relatedView);
+});
 
-	function renderRelatedSongs(songs) {
-		var view = '<ul>Related <i>(First one will play if nothing in queue)</i>:';
+function renderRelatedSongs(songs) {
+	var view = '<ul>Related <i>(First one will play if nothing in queue)</i>:';
 
-		for(var relatedId in songs) {
-			var relatedSong = songs[relatedId];
-			view += '<li>' + relatedSong.title + ' <button class="add-song" data-youtube-id="' + relatedSong.youtubeId + '">Add to queue</button></li>'
-		}
-
-		view += '</ul>';
-
-		return view;
+	for(var relatedId in songs) {
+		var relatedSong = songs[relatedId];
+		view += '<li>' + relatedSong.title + ' <button class="add-song" data-youtube-id="' + relatedSong.youtubeId + '">Add to queue</button></li>'
 	}
 
-	function renderSong(song) {
-		// Default votes to 0
-		song.votes = song.votes || 0;
+	view += '</ul>';
 
-		return '<a href="https://youtube.com/watch?v=' + song.youtubeId + '" target="_blank">' 
-			+ song.title 
-			+ '</a> - ' 
-			+ ((song.playTime) ? song.playTime : song.duration) + 'sec. '
-			+ '<button class="btn-vote" data-song-id="' + song.id + '">Vote up (' + song.votes + ')</button>'
-		;
-	}
+	return view;
+}
 
-	function play(thingie) {
-		$('iframe').attr('src', 'https://www.youtube.com/embed/' + thingie);
-	}
+function renderSong(song) {
+	// Default votes to 0
+	song.votes = song.votes || 0;
 
-	function addMessage(message) {
-		$('#text').append('<p>Message: '+ $('<div/>').text(message).html() +'</p>');
+	return '<a href="https://youtube.com/watch?v=' + song.youtubeId + '" target="_blank">' 
+		+ song.title 
+		+ '</a> - ' 
+		+ ((song.playTime) ? song.playTime : song.duration) + 'sec. '
+		+ '<button class="btn-vote" data-song-id="' + song.id + '">Vote up (' + song.votes + ')</button>'
+	;
+}
 
-		$('#text').scrollTop(999999999);
-	}
+function play(thingie) {
+	$('iframe').attr('src', 'https://www.youtube.com/embed/' + thingie);
+}
+
+function addMessage(message) {
+	$('#text').append('<p>Message: '+ $('<div/>').text(message).html() +'</p>');
+
+	$('#text').scrollTop(999999999);
+}
