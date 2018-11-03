a179cfbca6f040e74b5de70bb7299712b898c7f7
Veselin Bratanov <bratanov91@gmail.com>
2018-11-03 21:59:51 +0200

    Added message at the start of the chat (to help with first messages not seen with the sticky on) and Added a margin for the scrollbar when the sticky is on, will be a little off depending on the sticky size,

--- public/badass-client.js
+++ public/badass-client.js
@@ -351,10 +351,58 @@ socket.on('song_info', function(data) {
 
 	function resetStickyMessages() {
 		$('.c-chat-history__item--sticky').hide();
+		$('.c-chat-history__messages').removeClass('c-chat-history__messages--with-sticky');
 	}
 
 	function setStickyMessage() {
 		$('.c-chat-history__item--sticky').show();
+		$('.c-chat-history__messages').addClass('c-chat-history__messages--with-sticky');
+	}
+
+	function setHistoryStartMessage() {
+		var $chatHistoryStart = $('#chat-history-start');
+		var startMessages = [
+			'It starts with one',
+			'One thing, I don\'t know why',
+			'It doesn\'t even matter how hard you try',
+			'Keep that in mind',
+			'I designed this rhyme to explain in due time',
+			'All I know',
+			'Time is a valuable thing',
+			'Watch it fly by as the pendulum swings',
+			'Watch it count down to the end of the day',
+			'The clock ticks life away',
+			'It\'s so unreal',
+			'You didn\'t look out below',
+			'Watch the time go right out the window',
+			'Tryin\' to hold on, they didn\'t even know',
+			'I wasted it all just to watch you go',
+			'I kept everything inside',
+			'And even though I tried, it all fell apart',
+			'What it meant to me will eventually be a memory',
+			'Of a time when I tried so hard',
+			'One thing, I don\'t know why',
+			'It doesn\'t even matter how hard you try',
+			'Keep that in mind, I designed this rhyme',
+			'To remind myself how I tried so hard',
+			'It\'s part of the way you were mockin\' me',
+			'Actin\' like I was part of your property',
+			'Remembering all the times you fought with me',
+			'I\'m surprised it got so far',
+			'Things aren\'t the way they were before',
+			'You wouldn\'t even recognize me anymore',
+			'Not that you knew me back then',
+			'But it all comes back to me in the end',
+			'You kept everything inside',
+			'And even though I tried, it all fell apart',
+			'What it meant to me will eventually',
+			'Be a memory of a time when I tried so hard'
+		];
+		var chosenMessages = []
+		for (var i = 0; i < 3; i++) {
+			chosenMessages.push(startMessages.splice(parseInt(startMessages.length*Math.random()), 1)[0]);
+		}
+		$chatHistoryStart.html(`<i>${chosenMessages.join('<br />')}</i>`);
 	}
 
 	function setStickyMessagePosition(e) {
@@ -398,6 +446,8 @@ socket.on('song_info', function(data) {
 			setStickyMessage($songMessage);
 		}
 	}
+
+	setHistoryStartMessage();
 
 	// add sticky message
 	var $stickyMessage = renderer._cloneTemplate('t-system-message');
